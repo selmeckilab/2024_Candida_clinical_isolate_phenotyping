c2205eced1e63fb26f3bf78710520c9e7cdc8713
Nancy Scott <scot0854@umn.edu>
2023-11-27 12:05:48 -0600

    Fix (?) C. parapsilosis breakpoint, add redcap upload

--- scripts/redcap_eucast_calcs_api.R
+++ scripts/redcap_eucast_calcs_api.R
@@ -22,6 +22,13 @@ samples <- '58043'
 mic_results <- '58044'
 
 token <- "" # don't forget to delete before gh
+api_url <-  "https://redcap.ahc.umn.edu/api/"
+
+# add max concentration from MIC plates (below is standard MEC screening set-up)
+# change if needed
+max_flc <- 32
+max_amb <- 1.00
+max_mcf <- 1.00
 
 # function to import report from redcap
 import_report <- function(report_number) {
@@ -48,8 +55,8 @@ sample_info <- import_report(samples) %>%
 species <- sample_info %>%
     select(primary_id, genus_species)
 
-non_eucast_species <- c("Clavispora (Candida) lusitaniae", "Candida kefyr (Kluyveromyces marxianus)",
-                        "Candida nivariensis", "Candida orthopsilosis", "Candida utilis (Cyberlindnera jadinii)")
+non_eucast_species <- c("Candida lusitaniae", "Candida kefyr",
+                        "Candida nivariensis", "Candida orthopsilosis", "Candida utilis")
 
 # get MIC data and add species
 eucast_mic <- import_report(mic_results) %>%
@@ -62,35 +69,63 @@ eucast_mic$mic50 <- case_when(grepl(">", eucast_mic$mic50) ~ (as.numeric(sub("[^
 
 eucast_mic <- eucast_mic %>%
     filter(mic_media == "RPMI" & mic_temp=="35C") %>%
-    filter(mic_date %in% c(as.Date("2023-10-11"), as.Date("2023-10-17"))) %>%
+    filter(mic_date %in% c(as.Date("2023-11-21"), as.Date("2023-11-22"))) %>%
     group_by(genus_species) %>%
     mutate(eucast_breakpoint = case_when(drug!= "fluconazole" & genus_species %in% non_eucast_species ~ 3,
-                                         drug == "fluconazole" & genus_species == "Candida krusei (Pichia kudriavzevii)" ~ 4,
+                                         drug == "fluconazole" & genus_species == "Candida krusei" ~ 4,
                                          drug == "fluconazole" & genus_species == "Candida glabrata" & mic50 <= 16 ~ 1,
                                          drug == "fluconazole" & genus_species == "Candida glabrata" & mic50 > 16 ~ 2,
-                                         drug == "fluconazole" & !(genus_species %in% c("Candida glabrata", "Candida krusei (Pichia kudriavzevii)")) & mic50 <= 2 ~ 0,
-                                         drug == "fluconazole" & !(genus_species %in% c("Candida glabrata", "Candida krusei (Pichia kudriavzevii)"))  & between(mic50, 2.001, 4) ~ 1,
-                                         drug == "fluconazole" & !(genus_species %in% c("Candida glabrata", "Candida krusei (Pichia kudriavzevii)"))  & mic50 > 4 ~ 2,
+                                         drug == "fluconazole" & !(genus_species %in% c("Candida glabrata", "Candida krusei")) & mic50 <= 2 ~ 0,
+                                         drug == "fluconazole" & !(genus_species %in% c("Candida glabrata", "Candida krusei"))  & between(mic50, 2.001, 4) ~ 1,
+                                         drug == "fluconazole" & !(genus_species %in% c("Candida glabrata", "Candida krusei"))  & mic50 > 4 ~ 2,
                                          drug == "micafungin" & genus_species == "Candida albicans" & mic50 <= 0.016 ~ 0,
                                          drug == "micafungin" & genus_species == "Candida albicans" & mic50 > 0.016 ~ 2,
-                                         drug == "micafungin" & genus_species == "Candida glabrata" & mic50 <= 0.032 ~0,
-                                         drug == "micafungin" & genus_species == "Candida glabrata" & mic50 > 0.032 ~2,
-                                         drug == "micafungin" & genus_species == "Candida parapsilosis" & mic50 <= 2 ~0,
+                                         drug == "micafungin" & genus_species == "Candida glabrata" & mic50 <= 0.032 ~ 0,
+                                         drug == "micafungin" & genus_species == "Candida glabrata" & mic50 > 0.032 ~ 2,
+                                         drug == "micafungin" & genus_species == "Candida parapsilosis"  & mic50 <= max_mcf & max_mcf < 2 ~ 0,
                                          drug == "micafungin" & genus_species == "Candida parapsilosis" & mic50 > 2 ~ 2,
-                                         drug == "micafungin" & genus_species %in% c("Candida krusei (Pichia kudriavzevii)", "Candida tropicalis") ~ 3,
+                                         drug == "micafungin" & genus_species %in% c("Candida krusei", "Candida tropicalis") ~ 3,
                                          drug == "amphotericin B" & genus_species %in% c("Candida albicans",
                                                                                          "Candida dubliniensis",
                                                                                          "Candida glabrata",
-                                                                                         "Candida krusei (Pichia kudriavzevii)",
+                                                                                         "Candida krusei",
                                                                                          "Candida parapsilosis",
                                                                                          "Candida tropicalis") & mic50 <= 1 ~ 0,
                                          drug == "amphotericin B" & genus_species %in% c("Candida albicans",
                                                                                          "Candida dubliniensis",
                                                                                          "Candida glabrata",
-                                                                                         "Candida krusei (Pichia kudriavzevii)",
+                                                                                         "Candida krusei",
                                                                                          "Candida parapsilosis",
                                                                                          "Candida tropicalis") & mic50 > 1 ~ 2))
 
-write_csv(eucast_mic, "eucast_raw.csv")
+#write_csv(eucast_mic, "eucast_raw.csv")
 
-## TODO: add better date filters and create import function
+# Upload EUCAST categories to REDCap
+
+for(i in 1:length(eucast_mic$primary_id)){
+    
+    record <- c(
+        primary_id = eucast_mic$primary_id[i],
+        redcap_repeat_instrument = "mic_results",
+        redcap_repeat_instance = eucast_mic$redcap_repeat_instance[i],
+        eucast_breakpoint = eucast_mic$eucast_breakpoint[i]
+    )
+    
+    result_data <- toJSON(list(as.list(record)), auto_unbox=TRUE)
+    
+    formData <- list("token"=token,
+                     content='record',
+                     action='import',
+                     format='json',
+                     type='flat',
+                     overwriteBehavior='normal',
+                     forceAutoNumber='false',
+                     data=result_data,
+                     returnContent='count',
+                     returnFormat='json'
+    )
+    
+    response <- httr::POST(api_url, body = formData, encode = "form")
+    result <- httr::content(response)
+    print(result)
+}
