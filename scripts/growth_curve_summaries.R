b4b744d9df6ca53b2855da49853d4327dd3a81ff
Nancy Scott <scot0854@umn.edu>
2024-02-19 20:29:53 -0600

    Fix dataframe filtering and update to violin plot from boxplot

--- scripts/growth_curve_summaries.R
+++ scripts/growth_curve_summaries.R
@@ -23,7 +23,7 @@ mic_results <- '58044'
 growth_curves <- '58045'
 genes <- '58048'
 
-token <- '' # no gh
+token <- ''
 
 species_colors <- c("#88CCEE", "#999933", "#CC6677", "#44AA99", "#117733", "#332288",
                     "#882255","#BBBBBB", "#AA4499", "#DDCC77", "black")
@@ -53,16 +53,20 @@ sample_info <- import_report(samples) %>%
     filter(!primary_id %in% c("MEC103", "MEC113")) %>%
     filter(isolate_type == "clinical")
 
+sample_info$genus_species <- str_replace(sample_info$genus_species, "Candida", "C.")
+
 gc <- import_report(growth_curves) %>%
     filter(redcap_repeat_instrument != "NA", 
            !primary_id %in% c("AMS5122", "AMS5123")) %>%
     select(primary_id, redcap_repeat_instance, 
            gc_date, drug_used, 
            gc_temp, gc_time, 
-           k, r, t_gen, auc_l) %>% 
+           k, r, t_gen, auc_l) 
+
+gc <- gc %>% 
+    filter(drug_used=="None", gc_date > as.Date("2022-12-31")) %>% 
     left_join(sample_info %>% select(primary_id,genus_species,series_id), 
-              by = join_by(primary_id)) %>% 
-    filter(drug_used=="None", !series_id=="AH", gc_date > as.Date("2022-12-31"))
+              by = join_by(primary_id))
 
 ################################################################################
 # Data wrangling and summary plots.
@@ -118,32 +122,26 @@ r_metric <- ggplot(gc, aes(x=genus_species, y=r)) +
     xlab("Species") +
     ylab("Growth rate, r")
 
-tgen_metric <- ggplot(gc, aes(x=genus_species, y=t_gen)) + 
-    geom_boxplot(aes(fill=genus_species), 
-                 outlier.colour = NA, 
-                 width = 0.4, 
-                 alpha = 0.8) +
-    geom_dotplot(binaxis = "y", 
-                 binwidth = 0.015,
-                 stackdir = "center", 
-                 color = "grey36", 
-                 fill="grey36") + 
-    theme_bw() +
+tgen_metric <- ggplot(gc, aes(x=genus_species, y=t_gen, fill=genus_species)) + 
+    geom_violin() +
     scale_fill_manual(values=species_colors, guide = "none") +
+    geom_point(data = filter(gc, genus_species=="C. nivariensis")) +
+    theme_bw() +
+    scale_y_continuous(limits = c(0,3.5), breaks = c(0,1,2,3))+
     theme(axis.text.x = element_text(angle = 30, 
                                      face="italic", 
                                      hjust = 1, 
                                      vjust = 1)) +
     xlab("Species") +
-    ylab("Doubling time, t_gen")
+    ylab("Doubling time (hr)")
 
 stack_plots <- k_metric/r_metric
 
-ggsave(paste0("images/2023_growth_curves/",Sys.Date(),"_k_r_summaries.png"),
-       stack_plots,
+ggsave(paste0("images/2023_growth_curves/",Sys.Date(),"_doubling_time.png"),
+       tgen_metric,
        device=png, 
        bg="white", 
        dpi=300, 
-       width = 11.5, 
+       width = 7, 
        height=5, 
        units="in")
