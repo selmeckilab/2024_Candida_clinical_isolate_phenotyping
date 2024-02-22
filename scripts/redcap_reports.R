bda844d553ddbcbf70dfd743b75c7cbdd3f97cda
Nancy Scott <scot0854@umn.edu>
2024-02-22 14:38:28 -0600

    Add section for C. glabrata MLST report and plot

--- scripts/redcap_reports.R
+++ scripts/redcap_reports.R
@@ -9,8 +9,6 @@
 ##
 ## Email: scot0854@umn.edu
 ## ---------------------------
-## Notes:
-## ---------------------------
 ## load packages
 library(tidyverse)
 library(writexl)
@@ -34,7 +32,7 @@ patient_colors <- c(paletteer_d("ggsci::default_igv"),
                     "#cdcdb4",
                     "#155F83")
 
-# function to import report from redcap
+# Function to import report from redcap
 import_report <- function(report_number) {
   url <- "https://redcap.ahc.umn.edu/api/"
   formData <- list("token"=token,
@@ -57,6 +55,7 @@ sample_info <- import_report(samples) %>%
     filter(!primary_id %in% c("MEC103", "MEC113")) %>%
     filter(isolate_type == "clinical")
 
+################################################################################
 # CHEF gel results
 chef_done <- import_report(chef_data) %>%
     filter(redcap_repeat_instrument != "NA") %>%
@@ -70,25 +69,28 @@ todo <- sample_info %>%
     filter(isolate_type == "clinical") %>%
     anti_join(chef_done)
 
-# export chef-todo
+# Export chef-todo list
 #write_xlsx(todo,paste0(Sys.Date(),"_CHEF_todo.xlsx"))
 
+################################################################################
 # MIC and SMG results
 mic_info <- import_report(mic_results) %>%
     filter(redcap_repeat_instrument != "NA", mic_media == "RPMI") %>%
     select(primary_id, redcap_repeat_instance, drug, mic_date, mic50, eucast_breakpoint, smg) %>% 
     left_join((sample_info %>% select(primary_id, genus_species, series_id)), by=join_by(primary_id)) 
 
-# MIC to-do
+# MIC to-do list
 mic_to_do <- sample_info %>% 
     filter(isolate_type == "clinical") %>%
     anti_join(mic_info)
 
-# growth curve results
+################################################################################
+# Growth curve results
 gc <- import_report(growth_curves) %>%
     filter(redcap_repeat_instrument != "NA") %>%
     select(primary_id, redcap_repeat_instance, gc_date, drug_used, gc_temp, gc_time, k, r, t_gen, auc_l)
 
+################################################################################
 # MSI location if sequencing data exists
 seq_info <- import_report(avail_seq_data) %>%
     filter(redcap_repeat_instrument != "NA") %>%
@@ -97,13 +99,15 @@ seq_info <- import_report(avail_seq_data) %>%
                 values_from = c("msi_path_r1", "msi_path_r2", "msi_long_read_path"),
                 names_vary = "slowest")
 
-# snp data
+################################################################################
+# SNP data
 gene_vars <- import_report(genes) %>%
     filter(redcap_repeat_instrument != "NA") %>%
     select(primary_id, redcap_repeat_instance, gene, protein_change, alt_freq) %>%
     left_join((sample_info %>% select(primary_id, genus_species)))
 
-# mlst types
+################################################################################
+# C. albicans MLST types and plot
 albicans_sts <- import_report(calb_mlst) %>% 
     select(primary_id, st, aat1a_exact_match, acc1_exact_match, adp1_exact_match,
            mpib_exact_match, sya1_exact_match, vps13_exact_match, zwf1b_exact_match) %>% 
@@ -131,5 +135,36 @@ st_plot <- ggplot(albicans_sts %>% count(st, patient_code), aes(st, n, fill=pati
     theme(axis.text.x = element_text(angle = 90))
 
 ggsave("images/Calbicans/Calbicans_MLST_count.png", st_plot, 
+       device = png, dpi=300, bg="white",
+       width = 8, height = 5, units = "in")
+
+####################################################################
+# C. glabrata MLST types and plot
+glabrata_sts <- import_report(cglab_mlst) %>% 
+    select(primary_id, st, fks_exact_match, leu2_exact_match, nmt1_exact_match,
+           trp1_exact_match, ugp1_exact_match, ura3_exact_match) %>% 
+    mutate(across(st:ura3_exact_match, as.character)) %>%
+    mutate(concat_alleles=paste(fks_exact_match, leu2_exact_match, 
+                                nmt1_exact_match, trp1_exact_match, 
+                                ugp1_exact_match, ura3_exact_match, sep = "")) %>% 
+    mutate(st = case_when(is.na(st) ~ concat_alleles, .default = st)) %>% 
+    inner_join(sample_info %>% select(primary_id, patient_code), by=join_by(primary_id))
+
+glabrata_sts$patient_code <- as.character(glabrata_sts$patient_code)
+
+glabrata_st_summary <- glabrata_sts %>% 
+    count(st) %>% 
+    arrange(desc(n))
+
+st_plot <- ggplot(glabrata_sts %>% count(st, patient_code), aes(st, n, fill=patient_code))+
+    geom_bar(stat = "identity") +
+    scale_fill_manual(values=patient_colors, guide = "none") +
+    scale_x_discrete(limits = glabrata_st_summary$st) +
+    theme_minimal() +
+    ylab("Number of isolates") +
+    xlab("Sequence type") +
+    theme(axis.text.x = element_text(angle = 90))
+
+ggsave("images/Cglabrata/Cglabrata_MLST_count.png", st_plot, 
        device = png, dpi=300, bg="white",
        width = 8, height = 5, units = "in")
