5d830c4ddf8bf7cddd29998f54eb506c11707ec9
Nancy Scott <scot0854@umn.edu>
2024-03-22 17:47:31 -0500

    switch violin plot to beeswarm

--- redcap_reports/growth_curve_summaries.R
+++ redcap_reports/growth_curve_summaries.R
@@ -10,6 +10,7 @@ library(tidyverse)
 library(readxl)
 library(writexl)
 library(patchwork)
+library(beeswarm)
 
 # redcap report IDs
 samples <- '58043'
@@ -50,13 +51,13 @@ sample_info$genus_species <- str_replace(sample_info$genus_species, "Candida", "
 
 mic_info <- import_report(mic_results) %>%
     filter(redcap_repeat_instrument != "NA") %>%
-    select(primary_id, redcap_repeat_instance, drug, mic_media,mic_date, mic50, eucast_breakpoint, smg) %>% 
+    select(primary_id, redcap_repeat_instance, drug, mic_media,mic_date, mic50, eucast_breakpoint, smg, qc_ok) %>% 
     left_join((sample_info %>% 
                    select(primary_id, genus_species, series_id)), 
               by=join_by(primary_id))
 
 mic_info <- mic_info %>% 
-    filter(mic_media=="RPMI", !(primary_id %in% c("AMS5122","AMS5123")))
+    filter(mic_media=="RPMI", !(primary_id %in% c("AMS5122","AMS5123")), qc_ok=="Yes")
 
 mic_info$mic50 <- as.factor(mic_info$mic50)
 
@@ -66,13 +67,13 @@ mic_info$mic50 <- fct_relevel(mic_info$mic50, "0.016", "0.032", "0.064",
                               ">1", "2", "4", "8", "16", "32", ">32")
 
 mic_info <- mic_info %>% 
-    group_by(primary_id) %>% 
+    group_by(primary_id, drug) %>% 
     filter(mic_date==max(mic_date))
 
 
 mic_info <- mic_info %>%    
     select(primary_id, drug, mic_date, mic50, eucast_breakpoint, smg) %>% 
-    pivot_wider(names_from = drug, values_from = c(mic50, smg, eucast_breakpoint))
+    pivot_wider(names_from = drug, values_from = c(mic50, smg, eucast_breakpoint, mic_date))
     
 
 gc <- import_report(growth_curves) %>%
@@ -81,15 +82,13 @@ gc <- import_report(growth_curves) %>%
     select(primary_id, redcap_repeat_instance, 
            gc_date, drug_used, 
            gc_temp, gc_time, 
-           k, r, t_gen, auc_l) 
+           k, r, t_gen, auc_l, auc_e) 
 
 gc <- gc %>% 
-    filter(drug_used=="None", gc_date > as.Date("2022-12-31")) %>% 
+    group_by(primary_id) %>% 
+    filter(drug_used=="None", gc_date==max(gc_date), gc_time < 24.1) %>% 
     left_join(sample_info %>% select(primary_id,genus_species,series_id), 
               by = join_by(primary_id))
-
-gc <- gc %>% group_by(primary_id) %>% 
-    filter(redcap_repeat_instance ==max(redcap_repeat_instance))
 
 gc <- gc %>% 
     left_join(mic_info)
@@ -109,18 +108,10 @@ species_colors <- species_colors %>%
 
 gc$genus_species <- factor(gc$genus_species, levels = species_count$genus_species)
 
-k_metric <- ggplot(gc, aes(x=genus_species, y=k)) + 
-    geom_boxplot(aes(fill=genus_species), 
-                 outlier.colour = NA, 
-                 width = 0.4, 
-                 alpha = 0.8) +
-    geom_dotplot(binaxis = "y", 
-                 binwidth = 0.015,
-                 stackdir = "center", 
-                 color = "grey36", 
-                 fill="grey36") + 
+k_metric <- ggplot(gc, aes(x=genus_species, y=k, color = genus_species)) + 
+    geom_beeswarm(cex = 0.7) +
     theme_bw() +
-    scale_fill_manual(values=species_colors, guide = "none") +
+    scale_color_manual(values=species_colors, guide = "none") +
     theme(axis.text.x = element_blank()) +
     theme(axis.title.x = element_blank()) +
     #theme(axis.text.x = element_text(angle = 30, 
@@ -130,18 +121,10 @@ k_metric <- ggplot(gc, aes(x=genus_species, y=k)) +
     #xlab("Species") +
     ylab("Carrying capacity, k")
 
-r_metric <- ggplot(gc, aes(x=genus_species, y=r)) + 
-    geom_boxplot(aes(fill=genus_species), 
-                 outlier.colour = NA, 
-                 width = 0.4, 
-                 alpha = 0.8) +
-    geom_dotplot(binaxis = "y", 
-                 binwidth = 0.015,
-                 stackdir = "center", 
-                 color = "grey36", 
-                 fill="grey36") + 
+r_metric <- ggplot(gc, aes(x=genus_species, y=r, color = genus_species)) + 
+    geom_beeswarm(cex = 0.7) +
     theme_bw() +
-    scale_fill_manual(values=species_colors, guide = "none") +
+    scale_color_manual(values=species_colors, guide = "none") +
     theme(axis.text.x = element_text(angle = 30, 
                                      face="italic", 
                                      hjust = 1, 
@@ -151,50 +134,45 @@ r_metric <- ggplot(gc, aes(x=genus_species, y=r)) +
 
 stack_plots <- k_metric/r_metric
 
-tgen_metric <- ggplot(gc, aes(x=genus_species, y=t_gen, fill=genus_species)) + 
-    geom_violin() +
-    scale_fill_manual(values=species_colors, guide = "none") +
-    geom_point(data = filter(gc, genus_species=="C. nivariensis")) +
+auc_metric <- ggplot(gc, aes(x=genus_species, y=auc_e, color=genus_species)) + 
+    geom_beeswarm(size = 0.9, cex = 0.7) +
+    scale_color_manual(values=species_colors, guide = "none") +
     theme_bw() +
-    scale_y_continuous(limits = c(0,3.5), breaks = c(0,1,2,3))+
+    scale_y_continuous(limits = c(0,25))+
     theme(axis.text.x = element_text(angle = 30, 
                                      face="italic", 
                                      hjust = 1, 
                                      vjust = 1)) +
     xlab("Species") +
-    ylab("Doubling time (hr)")
+    ylab("AUC, 24H")
 
-ggsave(paste0("images/2023_growth_curves/",Sys.Date(),"_doubling_time.png"),
-       tgen_metric,
+ggsave(paste0("images/2023_growth_curves/",Sys.Date(),"_AUC_e.png"),
+       auc_metric,
        device=png, 
        bg="white", 
        dpi=300, 
        width = 7, 
-       height=5, 
+       height=5.5, 
        units="in")
 
-tgen_subset <- gc %>% 
-    filter(genus_species %in% c("C. albicans"), !is.na(eucast_breakpoint_fluconazole)) %>% 
-    ggplot(aes(x=eucast_breakpoint_fluconazole, y=t_gen, fill=genus_species)) + 
-    geom_violin() +
-    scale_fill_manual(values=species_colors, guide = "none") +
-    geom_dotplot(binaxis = "y", 
-                 binwidth = 0.015,
-                 stackdir = "center", 
-                 color = "grey36", 
-                 fill="grey36") + 
+auc_subset <- gc %>% 
+    filter(genus_species %in% c("C. glabrata"), !is.na(eucast_breakpoint_micafungin)) %>% 
+    ggplot(aes(x=eucast_breakpoint_micafungin, y=auc_e, color=genus_species)) + 
+    geom_beeswarm() +
+    scale_color_manual(values=species_colors, guide = "none") +
     theme_bw() +
-    scale_y_continuous(limits = c(0,3.5), breaks = c(0,1,2,3))+
-    #scale_x_discrete(labels =c("FLC Resistant", "FLC Intermediate"), limits=c("R", "I"))+
-    scale_x_discrete(labels=c("FLC Resistant", "FLC Sensitive"))+
+    scale_y_continuous(limits = c(0,25))+
+    #scale_x_discrete(labels =c("FLC Resistant", "FLC Sensitive, dose-dependent"), limits=c("R", "I"))+
+    #scale_x_discrete(labels=c("FLC Resistant", "FLC Sensitive"))+
+    scale_x_discrete(labels=(c("MCF Resistant", "MCF Sensitive"))) +
     theme(axis.text.x = element_text(#angle = 30, 
                                      #face="italic", 
                                      #hjust = 1, 
                                      #vjust = 1
         ),
           axis.title.x = element_text(face = "italic")) +
-    xlab("C. albicans") +
-    ylab("Doubling time (hr)")
+    xlab("C. glabrata") +
+    ylab("AUC, 24H")
 
-ggsave("images/2023_growth_curves/Calbicans_tgen_by_FLC_eucast.png", device = png, dpi=300, bg="white")
+ggsave("images/2023_growth_curves/Cglabrata_auc_by_MCF_eucast.png", auc_subset, device = png, dpi=300, bg="white")
 
